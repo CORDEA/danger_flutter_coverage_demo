6ee5d69c10f8fd9528ad89a78ffb22417c4587f1
Yoshihiro Tanaka <contact@cordea.jp>
2021-05-01 12:31:24 +0900

    restructure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:danger_flutter_coverage_demo/main.dart';
+import 'package:danger_flutter_coverage_demo/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
